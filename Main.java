e49bbad2a0c62c48684eaeb92a06c14d83259285
Andy Porras <andyporras6@gmail.com>
2021-07-21 21:28:32 -0600

    Código POO
    Este código para que funciones debe tener el código de operacion.

---
+++ Main.java
@@ -0,0 +1,17 @@
+package ClasesyObjetos;
+
+public class Main {
+    public static main(String [] args){
+        Operacion op = new Operacion();
+        
+        op.leerNumero();
+        op.sumar();
+        op.restar();
+        op.multiplicar();
+        op.dividir();
+        op.mostrarResultado();
+        
+        
+    }
+    
+}
