2f5c1760682b43a2ee4144cfa5c86543898f41a5
Andy Porras <andyporras6@gmail.com>
2021-07-23 11:01:51 -0600

    Código de POO, Modificadores
    este código debe instalarse en otro paquete distinto al de Clase1 y de la Clase2

---
+++ Clase3.java
@@ -0,0 +1,18 @@
+
+package paquete2;
+
+import paquete1.Clase1;
+
+/**
+ *
+ * @author Andy Porras
+ */
+public class Clase3 {
+    public static void main(String[] args){
+        Clase1 objeto1 = new Clase1();
+        
+        objeto1.atributo1= 15;
+        
+    }
+    
+}
