1a572e541097af2c48541b88287548bf041fc921
Andy Porras <andyporras6@gmail.com>
2021-07-23 11:00:05 -0600

    Código de POO, Modificadores

---
+++ Clase1.java
@@ -0,0 +1,21 @@
+
+package paquete1;
+
+/**
+ *
+ * @author Andy Porras
+ */
+public class Clase1 {
+    //Atributo
+    //int atributo1;
+    
+    public int atributo1;//se utiliza public para poder 
+    //a seder en otros paquetes
+    
+    //private int atributo1;
+   /* El private es para que el atributo
+    no se pueda a seder ni aunque este en el mismo
+    paquete.
+    */
+    
+}
---
+++ Clase2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+/**
+ *
+ * @author Andy Porras
+ */
+public class Clase2 {
+    public static void main(String[] args){  
+    Clase1 objeto1 = new Clase1();
+    
+    objeto1.atributo1=15;
+    }
+    
+}
